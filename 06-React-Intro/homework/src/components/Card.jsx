65ab3cbd8b8fc68b5a03494e028b0d1ec8d42da2
EnriqueLopez <ingeniero.enriquelopez@gmail.com>
2022-05-06 22:43:04 -0500

    Tarea 06-React Intro Terminada

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import Temp from "./Temp";
 
-export default function Card(props) {
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+        <button onClick = {onClose}>x</button>
+        <span>{name}</span>
+        
+        <div>
+            <Temp etiq = "Min" valor = {min} ></Temp>
+            <Temp etiq = "Max" valor = {max}></Temp>
+            <img src = {`http://openweathermap.org/img/wn/${img}@2x.png`} alt = "icono"/>
+        </div>
+    </div>
+  )
 };
---
+++ 06-React-Intro/homework/src/components/Temp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Temp({etiq, valor}) {     
+  return (
+    <div>
+        <span>{etiq}</span>
+        <span>{valor}</span>
+    </div>
+  )
+}
